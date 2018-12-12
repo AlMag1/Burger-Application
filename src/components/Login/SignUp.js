9ad944c26cd6afaeb7fa5f0acd19ef7e02a998a2
Aris Alexopoulos <alex.aris90@gmail.com>
2018-12-12 14:07:41 +0200

    upgraded form component

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -2,44 +2,113 @@ import React, { Component } from 'react';
 import classes from './SignUp.css';
 
 class SignUp extends Component {
+    state = {
+        users: {
+            username: '',
+            password: '',
+            firstname: '',
+            lastname: '',
+            floor: 0,
+            postalCode: 0,
+            address: '',
+            telephone: 0,
+            email: ''
+        }
+    }
+
+    inputChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.username = event.target.value;
+        this.setState({users});
+    }
+
+    passwordChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.password = event.target.value;
+        this.setState({users});
+    }
+
+    firstnameChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.firstname = event.target.value;
+        this.setState({users});
+    }
+
+    lastnameChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.lastname = event.target.value;
+        this.setState({users});
+    }
+
+    floorChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.floor = event.target.value;
+        this.setState({users});
+    }
+
+    postalCodeChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.postalCode = event.target.value;
+        this.setState({users});
+    }
+
+    addressChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.address = event.target.value;
+        this.setState({users});
+    }
+
+    telephoneChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.telephone = event.target.value;
+        this.setState({users});
+    }
+
+    emailChangeHandler = (event) => {
+        let users = {...this.state.users};
+        users.email = event.target.value;
+        this.setState({users});
+    }
+
     render() {
+        console.log(this.state.users);
         return (
             <div>
                 <form method="POST" >
                     <div className={classes.signUp}>
                         <div className={classes.fields}>
                             <label htmlFor="username">Username:</label>
-                            <input type="text" name="username" id="username" placeholder="Username" className={classes.credentials} />
+                            <input type="text" name="username" id="username" placeholder="Username" className={classes.credentials} onChange={this.inputChangeHandler} />
                             <label htmlFor="password">Password:</label>
-                            <input type="password" name="password" id="password" placeholder="Password" className={classes.credentials} />
+                            <input type="password" name="password" id="password" placeholder="Password" className={classes.credentials} onChange={this.passwordChangeHandler} />
                         </div>
                         <div className={classes.fields}>
                             
                         </div>
                         <div className={classes.fields}>
                             <label htmlFor="firstname">Firstname:</label>
-                            <input type="text" name="firstname" id="firstname" placeholder="Firstname" className={classes.credentials} />
+                            <input type="text" name="firstname" id="firstname" placeholder="Firstname" className={classes.credentials} onChange={this.firstnameChangeHandler} />
                             <label htmlFor="lastname">Lastname:</label>
-                            <input type="text" name="lastname" id="lastname" placeholder="Lastname" className={classes.credentials} />
+                            <input type="text" name="lastname" id="lastname" placeholder="Lastname" className={classes.credentials} onChange={this.lastnameChangeHandler} />
                         </div>
                         <div className={classes.fields}>
                             
                         </div>
                         <div className={classes.fields}>
                             <label htmlFor="floor">Floor:</label>
-                            <input type="number" name="floor" id="floor" placeholder="Floor" className={classes.credentials} />
+                            <input type="number" name="floor" id="floor" placeholder="Floor" className={classes.credentials} onChange={this.floorChangeHandler} />
                             <label htmlFor="postal-code">Postal Code:</label>
-                            <input type="text" name="postal-code" id="postal-code" placeholder="Postal Code" className={classes.credentials} />
+                            <input type="text" name="postal-code" id="postal-code" placeholder="Postal Code" className={classes.credentials} onChange={this.postalCodeChangeHandler} />
                         </div>
                         <div className={classes.fields}>
                             <label htmlFor="address">Address:</label>
-                            <input type="text" name="address" id="address" placeholder="Address" className={classes.credentials} />
+                            <input type="text" name="address" id="address" placeholder="Address" className={classes.credentials} onChange={this.addressChangeHandler} />
                         </div>
                         <div className={classes.fields}>
                             <label htmlFor="telephone">Telephone:</label>
-                            <input type="text" name="telephone" id="telephone" placeholder="Telephone" className={classes.credentials} />
+                            <input type="text" name="telephone" id="telephone" placeholder="Telephone" className={classes.credentials} onChange={this.telephoneChangeHandler} />
                             <label htmlFor="email">Email:</label>
-                            <input type="email" name="email" id="email" placeholder="E-mail" className={classes.credentials} />
+                            <input type="email" name="email" id="email" placeholder="E-mail" className={classes.credentials} onChange={this.emailChangeHandler} />
                         </div>
                         <div className={classes.fields}>
                             
--- src/components/Static/Footer.js
+++ src/components/Static/Footer.js
@@ -4,20 +4,20 @@ import classes from './Footer.css';
 class Footer extends Component {
     render() {
         return (
-            <footer className={classes.footer}>
+            <div className={classes.footer}>
                 <p className={classes.contact}>
                     <strong>Contact details</strong>
                 </p>
                 <p>
                     <strong>Address:</strong> 25 Nafpliou Str. Vouliagmeni, Greece
-          </p>
+                </p>
                 <p>
                     <strong>Phone:</strong> +30 2102201234
-          </p>
+                 </p>
                 <p>
                     <strong>Email:</strong> contact@burgerhub.com
-          </p>
-            </footer>
+                </p>
+            </div>
         );
     }
 }
